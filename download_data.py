38df90e059469edbb7fe565b7522395fbdf9b60b
Pavel Akhtyamov <akhtyamovpavel@gmail.com>
2022-11-28 14:22:00 +0300

    Download data script added

--- download_data.py
+++ download_data.py
@@ -2,4 +2,4 @@ from torchvision.datasets import CIFAR10
 from torchvision import transforms
 
 
-train_dataset = CIFAR10('./cifar10', train=True, transform=transforms.ToTensor(), download=True)
+train_dataset = CIFAR10('./datasets/cifar10', train=True, transform=transforms.ToTensor(), download=True)
